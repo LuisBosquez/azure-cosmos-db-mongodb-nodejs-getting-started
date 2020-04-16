93129d9d776f2f4a1aaf4873b7275d256e0af4de
Ubuntu <lbosq@lbosq-vm-ubuntu1804.avvs3ajwc1vuzkkxj1jjjcsn0h.xx.internal.cloudapp.net>
2020-04-16 00:28:30 +0000

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+  endpoint: "<Your Azure Cosmos account URI>",
+  key: "<Your Azure Cosmos account key>",
+  databaseId: "db",
+  containerId: "coll",
+};
+
+module.exports = config;
